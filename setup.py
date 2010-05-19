da702e3c8cd4d39d46c85fde3d6dd84860472fec
Byron Clark <byron@theclarkfamily.name>
2010-05-19 13:34:06 -0600

    Include the license.

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from distutils.core import setup
 
 setup(
     name='udiskie',
-    version='0.2.1',
+    version='0.3.0',
     description='Removable disk automounter for udisks',
     author='Byron Clark',
     author_email='byron@theclarkfamily.name',
     url='http://bitbucket.org/byronclark/udiskie',
+    license='MIT',
     packages=[
         'udiskie',
     ],
