9ebb25310f99b4a129c8efdfbd87d121051d66a6
Byron Clark <byron@theclarkfamily.name>
2011-05-12 21:30:43 -0600

    Prepare for more development.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='udiskie',
-    version='0.3.7',
+    version='0.3.8',
     description='Removable disk automounter for udisks',
     author='Byron Clark',
     author_email='byron@theclarkfamily.name',
