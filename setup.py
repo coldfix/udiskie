5da5d5ee03828011217afd30d22303e11e8b4ecd
Thomas Gläßle <t_glaessle@gmx.de>
2013-09-16 02:40:57 +0200

    Add long_description for distutils

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 # encoding: utf-8
 from distutils.core import setup
 
+with open('README.rst') as readme:
+    long_description = readme.read()
+
 setup(
     name='udiskie',
     version='0.4.2',
     description='Removable disk automounter for udisks',
+    long_description=long_description,
     author='Byron Clark',
     author_email='byron@theclarkfamily.name',
     maintainer='Thomas Gläßle',
