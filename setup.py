176f0d7c45f49170f84d0710adaf9fe84f9e5ff7
Thomas Gläßle <t_glaessle@gmx.de>
2013-12-27 02:05:43 +0100

    Bump version
    CHANGES:
    
    - cache device states to avoid some race conditions
    - show filesystem label in mount/unmount notifications
    - retry to unlock LUKS devices when wrong password was entered twice
    - show 'eject' only if media is available (udisks1 ejects only in this case)
    - (un-) mount/lock notifications shown even when operations failed
    - refactor internal API
    - experimental support for udisks2

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ except IOError:
 
 setup(
     name='udiskie',
-    version='0.5.3',
+    version='0.6.0',
     description='Removable disk automounter for udisks',
     long_description=long_description,
     author='Byron Clark',
