6798df4657730484549fdeaa13578c2d7e36f4eb
Jonas Große Sundrup <jonas@letopolis.de>
2014-02-27 20:03:44 +0100

    Add function in AutoMounter to handle mounting of LUKS-devices not opened by udiskie

--- udiskie/automount.py
+++ udiskie/automount.py
@@ -16,3 +16,13 @@ class AutoMounter(object):
     def media_added(self, udevice):
         self._mounter.add_device(udevice)
 
+    def device_changed(self, old_state, new_state):
+        """
+        Check whether is_external changed, then mount
+        """
+        # fixes usecase: mount luks-cleartext when opened by
+        # non-udiskie-software problem: in this case the device is not seen as
+        # external from the beginning and thus not mounted
+        if not old_state.is_external and new_state.is_external:
+            self._mounter.add_device(new_state)
+
