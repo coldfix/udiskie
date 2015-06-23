b5f453c48fcbee7a7bbd59a725f0604f29687125
Thomas Gläßle <t_glaessle@gmx.de>
2015-06-23 10:46:58 +0200

    Update copyright notices

--- udiskie/__init__.py
+++ udiskie/__init__.py
@@ -13,4 +13,4 @@ __maintainer__ = 'Thomas Gläßle'
 __maintainer_email__ = 't_glaessle@gmx.de'
 
 __license__ = 'MIT'
-__copyright__ = 'Copyright (c) 2010 Byron Clark'
+__copyright__ = '(c) 2010-2012 Byron Clark, (c) 2013-2015 Thomas Gläßle'
