5e5a506204f6d7bee413d4f46a4a53baabdb49a3
Thomas Gläßle <t_glaessle@gmx.de>
2017-04-11 16:17:17 +0200

    Update year in copyright notice

--- udiskie/__init__.py
+++ udiskie/__init__.py
@@ -13,4 +13,4 @@ __maintainer__ = 'Thomas Gläßle'
 __maintainer_email__ = 't_glaessle@gmx.de'
 
 __license__ = 'MIT'
-__copyright__ = '(c) 2010-2012 Byron Clark, (c) 2013-2016 Thomas Gläßle'
+__copyright__ = '(c) 2010-2012 Byron Clark, (c) 2013-2017 Thomas Gläßle'
