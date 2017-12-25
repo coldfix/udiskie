ed2654b0d911e855400ea428f5edbca3f41fd75d
Thomas Gläßle <t_glaessle@gmx.de>
2017-12-25 21:31:26 +0100

    Simplify udiskie.locale

--- udiskie/locale.py
+++ udiskie/locale.py
@@ -2,40 +2,16 @@
 I18n utilities.
 """
 
-import gettext
+from gettext import translation
 
 
-__all__ = ['_']
+_t = translation('udiskie', localedir=None, languages=None, fallback=True)
 
 
-class Translator:
-
-    """
-    Simple translation and message formatting utility.
-    """
-
-    @classmethod
-    def create(cls, domain, localedir=None, languages=None):
-        """
-        Create a new translator for the given domain.
-
-        Arguments are as in ``gettext.translation``.
-        """
-        t = gettext.translation(domain, localedir, languages, fallback=True)
-        g = t.gettext
-        return cls(g)
-
-    def __init__(self, gettext):
-        """Initialize a translator with the given gettext function."""
-        self._gettext = gettext
-
-    def __call__(self, text, *args, **kwargs):
-        """Translate and then and format the text with ``str.format``."""
-        msg = self._gettext(text)
-        if args or kwargs:
-            return msg.format(*args, **kwargs)
-        else:
-            return msg
-
-
-_ = Translator.create('udiskie')
+def _(text, *args, **kwargs):
+    """Translate and then and format the text with ``str.format``."""
+    msg = _t.gettext(text)
+    if args or kwargs:
+        return msg.format(*args, **kwargs)
+    else:
+        return msg
