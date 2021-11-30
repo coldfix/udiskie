aad93e91a2ae53ef928292bf6d8b2d668156ef64
Thomas Gläßle <t_glaessle@gmx.de>
2021-11-30 21:03:45 +0100

    Be smarter about trying to find localedir

--- udiskie/locale.py
+++ udiskie/locale.py
@@ -3,10 +3,23 @@ I18n utilities.
 """
 
 import os
+import sys
 from gettext import translation
 
 
-localedir = os.environ.get('TEXTDOMAINDIR')
+testdirs = [
+    # manual override:
+    os.environ.get('TEXTDOMAINDIR'),
+    # editable installation:
+    os.path.join(os.path.dirname(__file__), '../build/locale'),
+    # user or virtualenv installation:
+    os.path.join(sys.prefix, 'share/locale'),
+]
+testfile = 'en_US/LC_MESSAGES/udiskie.mo'
+localedir = next(
+    (d for d in testdirs if d and os.path.exists(os.path.join(d, testfile))),
+    None)
+
 _t = translation('udiskie', localedir, languages=None, fallback=True)
 
 
